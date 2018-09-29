c3cd989f19077accc9a5dec5909adf05957b43cd
noob <noob@road2guru.com>
2018-09-29 10:13:26 +0000

    Fix bug on makefile which still ref to old cmocka path

--- calc.mk
+++ calc.mk
@@ -1,38 +1,74 @@
+#
+# sample that requires `cmocka` in order to work
+# let's checkout `cmocka` at compile time, that means `cmocka` will
+# be added as dependency
+#
+# Workflow:
+# 1. Clone `cmocka` then reset to special commit that confirmed work
+# 2. Build `cmocka`
+# 3. Copy output lib `libcmocka.so*` to bin directory
+# 4. Build sample app that use `cmocka` lib which built from previous
+#    steps
+#
+
 CUR_DIR		:= $(shell pwd)
 BIN_DIR		:= $(CUR_DIR)/_bin_calc
 OBJ_DIR		:= $(CUR_DIR)/_build_calc
 SRC_DIR		:= $(CUR_DIR)/src
+CMOCKA_DIR	:= $(OBJ_DIR)/cmocka
 
-INC_FLG		:= -I$(CUR_DIR)/inc -I$(CUR_DIR)/../cmocka/include
+INC_FLG		:= -I$(CUR_DIR)/inc -I$(CMOCKA_DIR)/include
 
 TARGET		:= $(BIN_DIR)/calc-test
 SRCS		:= $(SRC_DIR)/calc-test.c $(SRC_DIR)/calc.c 
 
-CC		:= gcc
-CD		:= cd
-GIT		:= git
+CC			:= gcc
+CD			:= cd
+CP			:= cp -f
+GIT			:= git
 CMAKE		:= cmake
 MAKE		:= make
-RM		:= rm 
+RM			:= rm 
 MKDIR		:= mkdir
 ECHO		:= echo
+CHMOD		:= chmod
 CFLAGS		:= -Wall -g -O2 $(INC_FLG) 
-LDFLAGS		:= -lcmocka -L$(OBJ_DIR)/src
+LDFLAGS		:= -lcmocka -L$(BIN_DIR)
 
-all:	prepare $(OBJ_DIR)/src/libcmocka.so
-	@$(ECHO) "> compile $(SRCS)"
-	@$(CC) $(CFLAGS) $(SRCS) -o $(TARGET) $(LDFLAGS)
+all: $(TARGET)
+
+$(TARGET): $(SRCS) $(BIN_DIR)/libcmocka.so
+	@$(ECHO) "> compile $@"
+	@$(CC) $(CFLAGS) $(SRCS) -o $@ $(LDFLAGS)
+	@$(ECHO) "LD_LIBRARY_PATH=$(BIN_DIR) $@" > $(BIN_DIR)/run_all.sh
+	@$(CHMOD) +x $(BIN_DIR)/run_all.sh
+
+$(OBJ_DIR)/src/libcmocka.so: $(OBJ_DIR) $(CMOCKA_DIR)
+	@$(ECHO) "> build cmocka..."
+	@$(CD) $(OBJ_DIR) && $(CMAKE) cmocka 
+	@$(MAKE) -C $(OBJ_DIR) cmocka
+
+$(BIN_DIR)/libcmocka.so: $(OBJ_DIR)/src/libcmocka.so $(BIN_DIR)
+	@$(CP) $(OBJ_DIR)/src/libcmocka.so* $(BIN_DIR)
 
 $(OBJ_DIR)/cmocka:
-	$(GIT) clone git://git.cryptomilk.org/projects/cmocka.git $(OBJ_DIR)/cmocka
+	@$(ECHO) "> clone cmocka..."
+	@$(GIT) clone git://git.cryptomilk.org/projects/cmocka.git $@
+	
+	# make sure samples always works even cmocka may change in feature,
+	# let's checkout the commit that was confirmed working
+	@$(CD) $@ && $(GIT) reset --hard ae34d16
 
-$(OBJ_DIR)/src/libcmocka.so: $(OBJ_DIR)/cmocka
-	cd $(OBJ_DIR) && $(CMAKE) cmocka 
-	$(MAKE) -C $(OBJ_DIR) cmocka
+$(BIN_DIR):
+	@$(ECHO) "> create output directory for executables ($@)..."
+	@$(MKDIR) -p $@
 
-prepare:
-	@$(ECHO) "> create output directory($(BIN_DIR))..."
-	@$(MKDIR) -p $(BIN_DIR) $(OBJ_DIR)
+$(OBJ_DIR):
+	@$(ECHO) "> create output directory for objects ($@)..."
+	@$(MKDIR) -p $@
+
+run_all: $(TARGET)
+	LD_LIBRARY_PATH=$(BIN_DIR) $<
 
 clean:
 	@$(ECHO) "> remove $(BIN_DIR)"
